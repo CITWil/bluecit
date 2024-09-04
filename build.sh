03394b8341393e902b09d20be695fb91acd2b7f6
CITWil <93660053+CITWil@users.noreply.github.com>
2024-09-04 04:12:23 +0000

    Application install
    Librewolf
    MS Edge
    Cosmic Desktop

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
-
+### Repos
+curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Edge-ium\nbaseurl=https://packages.microsoft.com/yumrepos/edge\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/edge.repo > /dev/null
+wget https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
 
 ### Install packages
 
@@ -13,7 +18,10 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
+rpm-ostree install librewolf
+rpm-ostree install microsoft-edge-stable
+rpm-ostree install cosmic-desktop
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
