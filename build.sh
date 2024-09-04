650cbb8b0275e5c93c960312f997ba7ddbcd02bd
CITWil <93660053+CITWil@users.noreply.github.com>
2024-09-04 04:29:43 +0000

    Azure Tarballing

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Edge-ium\nbaseurl=https://packages.microsoft.com/yumrepos/edge\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/edge.repo > /dev/null
 wget https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
 
+### Azure Storage
+wget https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer-linux-x64.tar.gz -O ~/Downloads/StorageExplorer-linux-x64.tar.gz
+mkdir -p ~/azure-storage-explorer
+tar xvf StorageExplorer-linux-x64.tar.gz -C ~/azure-storage-explorer
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
