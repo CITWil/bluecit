155f928bbe363261cdf84a5ea07f757eb412b8a0
CITWil <93660053+CITWil@users.noreply.github.com>
2024-09-07 14:55:49 +0000

    Troubleshooting

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 ### Repos
 curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 # echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-echo -e "[code]\nname=Edge-ium\nbaseurl=https://packages.microsoft.com/yumrepos/edge\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/edge.repo > /dev/null
-wget https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
+# echo -e "[code]\nname=Edge-ium\nbaseurl=https://packages.microsoft.com/yumrepos/edge\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/edge.repo > /dev/null
+# wget https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
 
 ### Azure Storage
-wget https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer-linux-x64.tar.gz -O ~/Downloads/StorageExplorer-linux-x64.tar.gz
-mkdir -p ~/azure-storage-explorer
-tar xvf StorageExplorer-linux-x64.tar.gz -C ~/azure-storage-explorer
+#wget https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer-linux-x64.tar.gz -O ~/Downloads/StorageExplorer-linux-x64.tar.gz
+#mkdir -p ~/azure-storage-explorer
+#tar xvf StorageExplorer-linux-x64.tar.gz -C ~/azure-storage-explorer
 
 ### Install packages
 
@@ -25,8 +25,8 @@ tar xvf StorageExplorer-linux-x64.tar.gz -C ~/azure-storage-explorer
 # this installs a package from fedora repos
 # rpm-ostree install screen
 rpm-ostree install librewolf
-rpm-ostree install microsoft-edge-stable
-rpm-ostree install cosmic-desktop
+#rpm-ostree install microsoft-edge-stable
+#rpm-ostree install cosmic-desktop
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
